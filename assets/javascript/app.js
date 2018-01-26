726e17d040c281aaf8c75eab022aeeb8afa950f0
Richard Paez <Richardp809@gmail.com>
2018-01-25 19:54:35 -0500

    Created a main app object with separate objects for modules on the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,66 @@
 var hix = []
 $(function () {
-    var socket = io();
-    $('form').submit(function(){
-      var newMessage = $('#m').val();
-      if( newMessage != '' ){
-        socket.emit('chat message', newMessage);
-        $('#m').val('');
-      }
-      
-      return false;
-    });
-    socket.on('chat message', function(msg){
-      hix.unshift(msg);
-      if(hix.length>10){hix.splice(-1);}
-      $('#messages').empty();
-      for(let i=0 ; i<hix.length ; i++){
-          $('#messages').prepend($('<li>').text(hix[i]));
-      }
-    });
+  var socket = io();
+  $('form').submit(function () {
+    var newMessage = $('#m').val();
+    if (newMessage != '') {
+      socket.emit('chat message', newMessage);
+      $('#m').val('');
+    }
+
+    return false;
   });
+  socket.on('chat message', function (msg) {
+    hix.unshift(msg);
+    if (hix.length > 10) {
+      hix.splice(-1);
+    }
+    $('#messages').empty();
+    for (let i = 0; i < hix.length; i++) {
+      $('#messages').prepend($('<li>').text(hix[i]));
+    }
+  });
+});
+
+var app = {
+
+  priceHistoryModule : {
+
+    init: function () {
+      console.log("Price History Loaded");
+    }
+  },
+
+  pollModule : {
+
+    init: function () {
+      console.log("Poll Module loaded");
+    }
+  },
+
+  newsModule : {
+
+    init: function () {
+      console.log("News module loaded");
+    }
+  },
+
+  chatModule : {
+
+    init: function () {
+      console.log("chat module loaded");
+    }
+  },
+
+  startup : function(){
+
+    this.priceHistoryModule.init();
+    this.pollModule.init();
+    this.newsModule.init();
+    this.chatModule.init();
+
+  }
+
+}
+
+app.startup(); // main entry point of this application
